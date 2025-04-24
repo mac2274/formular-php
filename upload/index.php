6f3120e37bae76d3e4ea3303f81208ce2fb6c410
Maggi <yuen.maggi@gmail.com>
2025-04-24 11:34:53 +0200

    commit

---
+++ upload/index.php
@@ -0,0 +1,4 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileUpload" id="fileUpload">
+    <input type="submit" value="Upload" name="upload">
+</form>
