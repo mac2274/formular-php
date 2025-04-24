965937a31b885318c5636a15aa9e55540b9e1290
Maggi <yuen.maggi@gmail.com>
2025-04-24 11:35:45 +0200

    change

--- upload/index.php
+++
@@ -1,4 +0,0 @@
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="fileUpload" id="fileUpload">
-    <input type="submit" value="Upload" name="upload">
-</form>
