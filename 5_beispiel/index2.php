42c33489d71b00d740a5c0fcac471abbd5114ce7
Maggi <yuen.maggi@gmail.com>
2025-04-24 10:27:35 +0200

    form

---
+++ 5_beispiel/index2.php
@@ -0,0 +1,26 @@
+<?php 
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loginformular erstellen</title>
+</head>
+<body>
+    <h1>Logge dich bitte ein</h1>
+    <form action="" method="POST">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required>
+
+        <label for="password">Passwort</label>
+        <input type="password" name="passwort" id="password" required>
+
+        <input type="submit" value="Anmelden">
+    </form>
+</body>
+</html>
