e7a4a14509b7a550676bdd628b9b27662af7e58e
Maggi <yuen.maggi@gmail.com>
2025-04-28 09:51:33 +0200

    all

---
+++ 8_Ordnerlisten/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datei und Ordner auflisten</title>
+</head>
+<body>
+    
+</body>
+</html>
