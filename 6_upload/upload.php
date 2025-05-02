a4aa21732c3ad7fea5c402a29e4f179eb7d095e7
Maggi <yuen.maggi@gmail.com>
2025-05-02 08:38:17 +0200

    Fehlerbenennung

--- 6_upload/upload.php
+++ 6_upload/upload.php
@@ -8,22 +8,26 @@ if(isset($_POST["submit"]) && isset($_FILES["fileToUpload"])) { //mit  hat das s
     $fileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
     if($fileType != "jpg" && $fileType != "png" && $fileType != "jpeg" && $fileType != "gif" ) {
         echo "Es sind nur JPG, JPEG, PNG & GIF Dateien erlaubt.";
+        echo "Fehler 1";
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
             echo "Die Datei <strong>" . basename( $_FILES["fileToUpload"]["name"]). "</strong> wurde erfolgreich hochgeladen.";
         } else {
             echo "Es gab einen Fehler beim Hochladen Ihrer Datei.";
+            echo "Fehler 2";
         }
     }
 
     //eindeutige Prüfung und Aussage bei keinem Upload
 } elseif (!isset($_FILES["fileToUpload"])) {
     echo "Es wurde keine Datei mitgegeben.";
+    echo "Fehler 3";
     echo $_POST["fileToUpload"];
         //eindeutige Prüfung und Aussage bei nicht Nutzung des Formulars
 } elseif (!isset($_POST["submit"])) {
     echo "Es wurde nicht über das Formular abgesendet";
+    echo "Fehler 4";
 }
-echo $_POST["fileToUpload"];
+
 ?>
 
