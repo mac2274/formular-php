ee8191418db4106c175f3068544683736c2e6f7e
Maggi <yuen.maggi@gmail.com>
2025-05-02 12:07:00 +0200

    formatieren

--- 8_Ordnerlisten/exercise/index.php
+++ 8_Ordnerlisten/exercise/index.php
@@ -4,13 +4,16 @@ $verzeichnis = __DIR__ . "/";
 $erlaubteDateien = ["jpg","jpeg","png","gif"];
 
 foreach($verzeichnis as $datei){
-if($datei != "." && $datei != ".."){
-$pfad = $verzeichnis . "/" . $datei;
+    if($datei != "." && $datei != ".."){
+        $pfad = $verzeichnis . "/" . $datei;
 
-if(is_file($datei) && in_array(pathinfo($datei, PATHINFO_EXTENSION), $erlaubteDateien)){
-echo "<img src=".$pfad ."alt=".$datei."><br>";
+        if(is_file($datei) && in_array(pathinfo($datei, PATHINFO_EXTENSION), $erlaubteDateien)){
+            echo "<img src=".$pfad ."alt=".$datei."><br>";
+        }
+    }
 }
-}
-}
+
+echo $verzeichnis;
+echo $;
 ?>
 
