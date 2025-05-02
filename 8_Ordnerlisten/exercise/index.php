1fd2509a53e038ddc6d5db69b85a11cc9b20e6a2
Maggi <yuen.maggi@gmail.com>
2025-05-02 12:03:17 +0200

    ob die datei funktioiert?

---
+++ 8_Ordnerlisten/exercise/index.php
@@ -0,0 +1,16 @@
+<?php
+$verzeichnis = __DIR__ . "/"; 
+
+$erlaubteDateien = ["jpg","jpeg","png","gif"];
+
+foreach($verzeichnis as $datei){
+if($datei != "." && $datei != ".."){
+$pfad = $verzeichnis . "/" . $datei;
+
+if(is_file($datei) && in_array(pathinfo($datei, PATHINFO_EXTENSION), $erlaubteDateien)){
+echo "<img src=".$pfad ."alt=".$datei."><br>";
+}
+}
+}
+?>
+
