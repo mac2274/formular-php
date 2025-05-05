fce44bec9b9ed3502f79b39e0a99a543d81f0b4b
Maggi <yuen.maggi@gmail.com>
2025-05-05 08:25:57 +0200

    c

--- 8_Ordnerlisten/exercise/index.php
+++ 8_Ordnerlisten/exercise/index.php
@@ -1,5 +1,9 @@
 <?php
-$verzeichnis = __DIR__ . "/"; //nur der Pfad zum Inhalt
+phpinfo();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$verzeichnis = __DIR__ ; //nur der Pfad zum Inhalt
 
 $verzeichnisInhalt = scandir($verzeichnis); //lesen des Inhalts
 
@@ -9,12 +13,34 @@ foreach($verzeichnisInhalt as $datei){
     if($datei != "." && $datei != ".."){
         $pfad = $verzeichnis . "/" . $datei;
 
-        if(is_file($datei) && in_array(pathinfo($datei, PATHINFO_EXTENSION), $erlaubteDateien)){
-            echo "<img src=".$pfad ."alt=".$datei."><br>";
+        if(is_file($pfad) && in_array(pathinfo($datei, PATHINFO_EXTENSION), $erlaubteDateien)){
+            echo "<img style='width:100px;height:auto;' src='$datei' alt='$datei'><br>"; //hier habe ich den Pfad mit $pfad angegeben, aber hier musst $datei, also der relative Dateiname rein
+
+            if (!function_exists('exif_read_data')) {
+                die("EXIF-Erweiterung ist nicht aktiviert!");
+            }
+echo $pfad;
+            $exif = exif_read_data($pfad);
+            if(isset($exif)){
+                echo "IST DA";
+            } else {
+                echo "GEHT NICHT";
+            };
+echo "HALLO";
+
+
+            echo "Bildgröße: " . $exif['COMPUTED']['Width'] . "x" . $exif['COMPUTED']['Height'] . "<br>";
+            echo "Aufnahmedatum: " . $exif['DateTimeOriginal'] . "<br>";
+
+            $thumbnail = exif_thumbnail($pfad, $thumbWidth, $thumbHeight, $thumbType);
+                if ($thumbnail !== false) {
+                    echo "<img src='data:image/jpeg;base64," . base64_encode($thumbnail) . "'alt='Thumbnail'><br>";
+                } else {
+                    echo "Kein Thumbnail verfügbar<br>";
+                }
         }
     }
 }
 
-echo $$pfad;
 ?>
 
